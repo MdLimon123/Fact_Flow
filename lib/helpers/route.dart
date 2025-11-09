04cdb0fa5379347f581190040b39266c8a886614
Md Limon Islam <limon222019@gmail.com>
2025-11-09 11:38:10 +0600

    initial commit

---
+++ lib/helpers/route.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+
+
+import '../views/screen/Home/home_screen.dart';
+import '../views/screen/Profile/profile_screen.dart';
+import '../views/screen/Splash/splash_screen.dart';
+
+
+class AppRoutes{
+
+  static String splashScreen="/splash_screen";
+  static String homeScreen="/home_screen";
+  static String profileScreen="/profile_screen";
+
+
+ static List<GetPage> page=[
+    GetPage(name:splashScreen, page: ()=>const SplashScreen()),
+    GetPage(name:homeScreen, page: ()=>const HomeScreen(),transition:Transition.noTransition),
+
+    GetPage(name:profileScreen, page: ()=>const ProfileScreen(),transition: Transition.noTransition),
+  ];
+
+
+
+}
---
+++ lib/views/screen/Splash/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:fact_flow/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/image/app_logo.png',
+              width: 190,
+              height: 150,
+            ),
+          ),
+
+ 
+
+          Text(
+            "Verify, Truth, Instantly",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              color: AppColors.textColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
